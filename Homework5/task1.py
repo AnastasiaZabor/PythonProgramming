4597f962a84a96f0e12e25999d613739cc9f1cf5
AnastasiaZabor <anastasiazaborskaa@gmail.com>
2022-09-19 12:36:14 +0700

    task1

---
+++ Homework5/task1.py
@@ -0,0 +1,18 @@
+# 1. Напишите программу, удаляющую из текста все слова, содержащие "абв".
+# 'Мы неабв очень любим Питон иабв Джавабв'
+# 'Мы очень любим Питон'
+
+def delet_all_word_from_the_text(string1:str, string2:str):
+    words = string1.split(" ")
+    print(words)
+    new_str = ""
+
+    for word in words:
+        if word.find(string2) == -1:
+            new_str += f'{word} '
+    return new_str
+
+string1 = input('Введите строку: ')
+string2 = input('Введите слово: ')
+new_str = delet_all_word_from_the_text(string1,string2)
+print(new_str)
